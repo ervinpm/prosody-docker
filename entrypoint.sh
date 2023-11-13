e3bfbb5e7c393067cd2080f7fe073a9188735da1
Ervin Marasigan <ervinpm@gmail.com>
2023-11-13 07:16:40 -0600

    fix: root user mod failing kubernetes deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,6 @@
 set -e
 
 data_dir_owner="$(stat -c %u "/var/lib/prosody/")"
-if [[ "$(id -u prosody)" != "$data_dir_owner" ]]; then
-    usermod -u "$data_dir_owner" prosody
-fi
 if [[ "$(stat -c %u /var/run/prosody/)" != "$data_dir_owner" ]]; then
     chown "$data_dir_owner" /var/run/prosody/
 fi
